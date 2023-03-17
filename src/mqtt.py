9dd30c1b75e118a46d75d71b34cc026cb3a3e6ee
OMGeeky <aalaalgmx@gmail.com>
2023-03-17 09:00:31 +0100

    Simplify subscribe callback

--- src/mqtt.py
+++ src/mqtt.py
@@ -1,4 +1,5 @@
 import random
+from typing import Callable
 from paho.mqtt import client as mqtt_client
 import json
 
@@ -14,47 +15,67 @@ class MqttSendError(ConnectionError):
 
 
 class MqttClient:
-    def __init__(self, broker: str, topic: str, subscribe_callback, username: str | None = None, password: str | None = None, client_id: str | None = None) -> None:
-        self._broker = broker
-        self._port = 1883
-        self._topic = "/gsog/shopping"  # TODO: settings
+    def __init__(
+        self,
+        broker: str,
+        port: int,
+        topic: str,
+        subscribe_callback: Callable[[dict, str], None],
+        username: str | None = None,
+        password: str | None = None,
+        client_id: str | None = None,
+    ) -> None:
+        self.__broker = broker
+        self.__port = port
+        self.__topic = topic
         self.__client_id = client_id
         if self.__client_id is None:
-            self.__client_id = f'python-mqtt-{random.randint(0, 1000)}'
+            self.__client_id = f"python-mqtt-{random.randint(0, 1000)}"
         self.__username = username
         self.__password = password
         self.__client: mqtt_client.Client | None = None
         self.__subscribe_callback = subscribe_callback
 
+    def parse_callback(self, client, userdata, msg, callback: Callable):
+
+        if callback is None:
+            callback = self.__subscribe_callback
+        x = json.loads(msg.payload.decode())
+        print(x)
+        print(userdata)
+        callback(x, msg.topic)
+
     @staticmethod
-    def on_connect(client, userdata, flags, rc) -> None:
+    def on_connect(_client, _userdata, _flags, rc) -> None:
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code", rc)
 
-    def publish(self, msg: str, topic: str | None = None) -> None:
+    def publish(self, msg: dict, topic: str | None = None) -> None:
         if self.__client is None:
             self.connect()
             if self.__client is None:
                 raise MqttConnectionError()
 
         if topic is None:
-            topic = self._topic
+            topic = self.__topic
 
         result = self.__client.publish(topic, msg)
         status = result[0]
         if status != 0:
             raise MqttSendError()
 
-    def subscribe(self) -> None:
+    def subscribe(self, callback=None) -> None:
         if self.__client is None:
             self.connect()
             if self.__client is None:
                 raise MqttConnectionError()
 
-        self.__client.subscribe(self._topic)
-        self.__client.on_message = self.__subscribe_callback
+        self.__client.subscribe(self.__topic)
+        self.__client.on_message = lambda client, userdata, msg: self.parse_callback(
+            client, userdata, msg, callback
+        )
 
     def connect(self) -> None:
         self.__client = mqtt_client.Client(self.__client_id)
@@ -62,7 +83,7 @@ class MqttClient:
             self.__client.username_pw_set(self.__username, self.__password)
 
         self.__client.on_connect = self.on_connect
-        self.__client.connect(self._broker, self._port)
+        self.__client.connect(self.__broker, self.__port)
         self.__client.loop_start()
 
     def disconnect(self) -> None:
@@ -77,19 +98,20 @@ class MqttClient:
 
 def sample2():
 
-    broker = 'broker.hivemq.com'
+    broker = "broker.hivemq.com"
     topic = "/gsog/shopping"
     username = None
     password = None
 
-    def on_message(client, userdata, msg) -> None:
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-    client = MqttClient(broker, topic, on_message, username, password)
+    def on_message(msg: dict, topic: str) -> None:
+        print(f"Received `{msg}` from `{topic}` topic")
+
+    client = MqttClient(broker=broker, port=1883,topic=topic,subscribe_callback= on_message,username= username,password= password)
     client.connect()
     count = 0
     while True:
         text = input("Press enter to send a message... (q to quit)")
-        if text == 'q':
+        if text == "q":
             break
 
         count += 1
@@ -108,5 +130,5 @@ def sample2():
     input("Press enter to quit...\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sample2()
