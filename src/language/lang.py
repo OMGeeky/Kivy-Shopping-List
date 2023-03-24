914da08eb51ceb872fd68bbb20ee6d4ae4073cb3
Jan <MapManagement@protonmail.com>
2023-03-24 20:39:00 +0100

    Add missing translations, refactor

--- src/language/lang.py
+++ src/language/lang.py
@@ -1,46 +1,42 @@
-
-# from data import AppSettings
-
 import json
 from pathlib import Path
 from typing import Dict, Optional
 
 
-LANGUAGES = { "DE": "Deutsch", "EN": "English", "FR": "Francais" }
-LANGUAGE_FOLDER = Path('res', "lang")
+LANGUAGES = {"DE": "Deutsch", "EN": "English", "FR": "Francais"}
+LANGUAGE_FOLDER = Path("res", "lang")
 
 
 class TranslationProvider:
     last_language_key: Optional[str] = None
-    last_language_dict: Optional[Dict[str,str]] = None
+    last_language_dict: Optional[Dict[str, str]] = None
 
     @classmethod
     def get_language_file(cls, language: str) -> Dict[str, str]:
         if language not in list(LANGUAGES.keys()):
             raise ValueError('invalid language key: "{language}"')
-        
+
         if cls.last_language_key == language and cls.last_language_dict is not None:
             return cls.last_language_dict
-            
-        path = Path(LANGUAGE_FOLDER, f'{language}.json' )
+
+        path = Path(LANGUAGE_FOLDER, f"{language}.json")
         try:
-            with open(path, 'r', encoding='utf-8') as f:
+            with open(path, "r", encoding="utf-8") as f:
                 result = json.load(f)
                 cls.last_language_key = language
                 cls.last_language_dict = result
                 return result
-            
+
         except FileNotFoundError:
             print(f'Language not found: "{language}"')
             raise
 
     @classmethod
-    def get_translated(cls, key:str, language:str) -> str:
-
+    def get_translated(cls, key: str, language: str) -> str:
         lang_dict = cls.get_language_file(language)
         result = lang_dict.get(key, None)
         if result:
             return result
 
-        print(f'unsuccesful try to get {key} in language {language}')
+        print(f"unsuccesful try to get {key} in language {language}")
         return f'key not found for language: \nkey:"{key}"\nlanguage:"{language}"'
--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 import json
+import os
 from typing import Optional
 from data import AppSettings
 from language import TranslationProvider, LANGUAGES
 from pathlib import Path
 
-from kivy.app import App
-from kivy.properties import ListProperty, StringProperty, BooleanProperty, ObjectProperty
+import kivy.utils
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
-import kivy.utils
 
 from kivymd.app import MDApp
 from kivymd.uix.button import MDFlatButton
@@ -20,11 +20,12 @@ from kivymd.uix.list import OneLineAvatarIconListItem
 from kivymd.toast import toast
 
 FILES_PATH = Path("files")
-settings_file_path = Path(FILES_PATH,'settings.json')
-entries_file_path = Path(FILES_PATH, 'entries.json')
+settings_file_path = Path(FILES_PATH, "settings.json")
+entries_file_path = Path(FILES_PATH, "entries.json")
 
-if kivy.utils.platform not in ['android', 'ios']:
+if kivy.utils.platform not in ["android", "ios"]:
     Window.size = (400, 800)
+
 
 class ShoppingEntry(OneLineAvatarIconListItem):
     edit_dialog = None
@@ -44,35 +45,44 @@ class ShoppingEntry(OneLineAvatarIconListItem):
 
         # SPRACHE
         buttons = [
-            MDFlatButton(text='Abbrechen', on_release=self.close_edit_popup),
-            MDFlatButton(text='Bestätigen', on_release=lambda args: self.save_changes(args)),
+            MDFlatButton(
+                text=self.get_translated("cancel"),
+                on_release=self.close_edit_popup
+            ),
+            MDFlatButton(
+                text=self.get_translated("confirm"),
+                on_release=lambda args: self.save_changes(args)
+            ),
         ]
-        # SPRACHE
+
         self.edit_dialog = MDDialog(
-            title='Eintrag ändern',
-            type='custom',
+            title=self.get_translated("edit_entry"),
+            type="custom",
             content_cls=AddDialog(self.text),
             buttons=buttons,
         )
 
         self.edit_dialog.open()
 
-    def save_changes(self, *args):
+    def save_changes(self, *_):
         if self.edit_dialog is None:
             return
 
         changed_text = self.edit_dialog.content_cls.ids["shopping_entry_text"].text
         if not changed_text or len(changed_text) == 0:
-            # SPRACHE
-            toast("The entry text cannot be empty.")
+            toast(self.get_translated("empty_text_alert"))
             return
 
         self.text = changed_text
 
         self.close_edit_popup()
 
-    # *args ist noetig, da weitere Parameter mitgegeben werden, die aber nicht genutzt werden
-    def close_edit_popup(self, *args):
+    def get_translated(self, key: str) -> str:
+        settings = AppSettings.get_or_create()
+        return TranslationProvider.get_translated(key, settings.language)
+
+    # *args bzw. *_ ist noetig, da weitere Parameter mitgegeben werden, die aber nicht genutzt werden
+    def close_edit_popup(self, *_):
         if not self.edit_dialog:
             return
 
@@ -86,27 +96,27 @@ class AddDialog(MDBoxLayout):
     def __init__(self, text="", **kwargs):
         super().__init__(**kwargs)
         self.text = text
-    
-    def get_translated(self, key:str)->str:
+
+    def get_translated(self, key: str) -> str:
         settings = AppSettings.get_or_create()
         return TranslationProvider.get_translated(key, settings.language)
 
+
 class ShoppingEntryScreen(Screen):
-    settings : Optional[AppSettings] = None
+    settings: Optional[AppSettings] = None
     add_dialog = None
 
-    def on_bestaetigen(self, *args):
+    def on_bestaetigen(self, *_):
         if self.add_dialog is None:
             return
-        
-        text = self.add_dialog.content_cls.ids['shopping_entry_text'].text
+
+        text = self.add_dialog.content_cls.ids["shopping_entry_text"].text
         if not text or len(text) == 0:
-            # SPRACHE
-            toast("The entry text cannot be empty.")
+            toast(self.get_translated("empty_text_alert"))
             return
 
         self.add_shopping_entry(text)
-        
+
     def open_add_popup(self):
         if self.add_dialog:
             return
@@ -114,20 +124,25 @@ class ShoppingEntryScreen(Screen):
         language = self.get_settings().language
 
         buttons = [
-            MDFlatButton(text=TranslationProvider.get_translated('cancel', language), on_release=self.close_add_popup),
-            MDFlatButton(text=TranslationProvider.get_translated('confirm', language), on_release=lambda args: self.on_bestaetigen(args)),
+            MDFlatButton(
+                text=TranslationProvider.get_translated("cancel", language),
+                on_release=self.close_add_popup,
+            ),
+            MDFlatButton(
+                text=TranslationProvider.get_translated("confirm", language),
+                on_release=lambda args: self.on_bestaetigen(args),
+            ),
         ]
         self.add_dialog = MDDialog(
-            title=TranslationProvider.get_translated('add_entry', language),
-            type='custom',
+            title=TranslationProvider.get_translated("add_entry", language),
+            type="custom",
             content_cls=AddDialog(),
             buttons=buttons,
         )
 
         self.add_dialog.open()
 
-    # *args ist noetig, da weitere Parameter mitgegeben werden, die aber nicht genutzt werden
-    def close_add_popup(self, *args):
+    def close_add_popup(self, *_):
         if not self.add_dialog:
             return
 
@@ -135,7 +150,7 @@ class ShoppingEntryScreen(Screen):
         self.add_dialog = None
 
     def add_shopping_entry(self, text):
-        self.ids['shopping_list'].add_widget(ShoppingEntry(text=text))
+        self.ids["shopping_list"].add_widget(ShoppingEntry(text=text))
         self.close_add_popup()
 
     def export_entries_to_json(self):
@@ -148,28 +163,35 @@ class ShoppingEntryScreen(Screen):
         try:
             if not FILES_PATH.is_dir():
                 FILES_PATH.mkdir()
-            with open(entries_file_path, "x") as json_file:
+
+            if os.path.exists(entries_file_path):
+                access_mode = "w"
+            else:
+                access_mode = "x"
+
+            with open(entries_file_path, access_mode) as json_file:
                 json.dump(settings_dict, json_file, indent=4)
-        except FileExistsError:
-            with open(entries_file_path, "w") as json_file:
-                json.dump(settings_dict, json_file, indent=4)
+
+        except OSError:
+            return
 
     def navigate_to_settings(self):
-        self.manager.transition.direction = 'left'
-        self.manager.current = 'settings'    
-        self.settings = None # reset settings to reload them after change
+        self.manager.transition.direction = "left"
+        self.manager.current = "settings"
+        self.settings = None  # reset settings to reload them after change
 
-    def load_settings(self)->None:
+    def load_settings(self) -> None:
         if not self.settings:
             self.settings = AppSettings.get_or_create()
 
     def get_settings(self) -> AppSettings:
         self.load_settings()
-        return self.settings # type: ignore
-    
+        return self.settings  # type: ignore
+
     def get_translated(self, key) -> str:
         language = self.get_settings().language
-        return TranslationProvider.get_translated(key,language)
+        return TranslationProvider.get_translated(key, language)
+
 
 class SettingsScreen(Screen):
     settings = ObjectProperty(AppSettings(), rebind=True)
@@ -185,7 +207,8 @@ class SettingsScreen(Screen):
                 "text": f"{LANGUAGES.get(language_key)}",
                 "height": dp(56),
                 "on_release": lambda x=language_key: self.set_item(x),
-            } for language_key in LANGUAGES.keys()
+            }
+            for language_key in LANGUAGES.keys()
         ]
 
         self.menu = MDDropdownMenu(
@@ -194,12 +217,12 @@ class SettingsScreen(Screen):
             position="center",
             width_mult=4,
         )
-        
+
         self.menu.bind()
 
     def navigate_to_shopping_list(self):
-        self.manager.transition.direction = 'right'
-        self.manager.current = 'shopping'
+        self.manager.transition.direction = "right"
+        self.manager.current = "shopping"
 
     def show_languages_menu(self):
         self.menu.open()
@@ -208,27 +231,29 @@ class SettingsScreen(Screen):
         language = LANGUAGES.get(language_key)
         self.ids.language_drop_down.set_item(language)
         self.menu.dismiss()
-    
+
     def get_translated(self, key) -> str:
         if self.settings is None:
             self.settings = AppSettings.get_or_create()
 
         language = self.settings.language
-        return TranslationProvider.get_translated(key,language)
-            
+        return TranslationProvider.get_translated(key, language)
+
+
 class ShoppingListApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Gray"
         self.theme_cls.theme_style = "Light"
         # TODO @Tom Theme anpassen
-        self.title = 'Shopping List App'
+        self.title = "Shopping List App"
 
         sm = ScreenManager()
-        sm.add_widget(ShoppingEntryScreen(name='shopping'))
-        sm.add_widget(SettingsScreen(name='settings'))
+        sm.add_widget(ShoppingEntryScreen(name="shopping"))
+        sm.add_widget(SettingsScreen(name="settings"))
 
         return sm
-                   
+
+
 if __name__ == "__main__":
     app = ShoppingListApp()
     app.run()
